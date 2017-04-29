119323bab161c4022b50a3028b1b46f9378e5e4e
tomer <tomerg@codevalue.net>
2017-04-29 14:31:37 +0300

    add frontend (register ui)

---
+++ frontend/frontend-service.js
@@ -0,0 +1,87 @@
+"use strict"
+
+var PORT = process.env.PORT || process.argv[2] || 0
+var HOST = process.env.HOST || process.argv[3] || 0
+var BASES = (process.env.BASES || process.argv[4] || '').split(',')
+var SILENT = process.env.SILENT || process.argv[5] || 'true'
+
+var hapi       = require('hapi')
+var chairo     = require('chairo')
+var vision     = require('vision')
+var inert      = require('inert')
+var handlebars = require('handlebars')
+var _          = require('lodash')
+var moment     = require('moment')
+var Seneca     = require('seneca')
+var Rif        = require('rif')
+
+
+var server = new hapi.Server()
+var rif = Rif()
+
+
+var host = rif(HOST) || HOST
+
+
+server.connection({
+    port: PORT,
+    host: host
+})
+
+server.register( vision )
+server.register( inert )
+
+server.register({
+  register:chairo,
+  options:{
+    seneca: Seneca({
+      tag: 'frontend',
+      internal: {logger: require('seneca-demo-logger')},
+      debug: {short_logs:true}
+    })
+        .use('entity')
+  }
+})
+
+server.register({
+  register: require('wo'),
+  options:{
+    bases: BASES,
+    route: [
+        {path: '/frontend'},
+    ],
+    sneeze: {
+      host: host,
+      silent: JSON.parse(SILENT),
+      swim: {interval: 1111}
+    }
+  }
+})
+
+
+server.views({
+  engines: { html: handlebars },
+  path: __dirname + '/www',
+  layout: true
+})
+
+
+server.route({
+  method: 'GET', path: '/frontend',
+  handler: function( req, reply )
+  {
+      reply.view('frontend')
+  }
+})
+
+
+server.seneca.use('mesh',{
+    bases:BASES,
+    host:host
+})
+
+server.start(function(){
+  console.log('frontend',server.info.host,server.info.port)
+})
+
+
